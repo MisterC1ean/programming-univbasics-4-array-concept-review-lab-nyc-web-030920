9e8977db4e3bf3edad788566abf94e0f0cecac39
Robert Keller <robert_keller44@icloud.com>
2019-12-24 20:02:34 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,10 +10,10 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
+  x = 0
   array.length.times {|index|
-  if array[index] < x 
+  if array[index] > x 
     x = array[index]
-    
   }
 end
 
